448fac80d2344ba3fe88e51383a62a4513a8c5d8
Antonio <anthonymartz17@hotmail.com>
2021-03-02 07:27:04 -0500

    added li for tasks

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
+<div class="container">
+  
+<app-searchBox></app-searchBox>
+<app-tasks></app-tasks>
+  
+
+</div>
 
 </template>
 
 <script>
+import inputbox from './components/inputBox'
+import tasks from './components/tasks'
+
 export default {
   
+  components:{
+    'app-searchBox': inputbox,
+    'app-tasks': tasks
+  },
   data () {
     return {
       
     }
   }
+  
 }
 </script>
 
 <style>
+body{
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+  background: #290149;
+  
+}
+.container{
+  width: 40%;
+  height: 70vh;
+  border: 2px solid white;
+  border-radius: 5px;
+  
+  
+  
 
+  
+}
 
 </style>
